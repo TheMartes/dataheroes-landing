a5b34cb4556efe15258aaa1bd59395ceca7bbc81
TheMartes <hello@themartes.com>
2022-08-17 11:29:50 +0200

    fix: favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,10 @@ const Home: NextPage = () => {
       <Head>
         <title>DataHeroes</title>
         <meta name="description" content="Dataheroes landing page." />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧠</text></svg>"
+        ></link>
       </Head>
 
       <main className="flex justify-center items-center h-screen flex-col">
